ed37734d449530c90c5cfeb44d9eefc4a241c061
Ivo Birolini <ivolvaz@hotmail.com>
2017-11-03 17:23:45 -0200

    Ranking alterado
    Excluir a página login e já deixei encaminhada a página do ranking.

--- pages/ranking.php
+++ pages/ranking.php
@@ -1,3 +1,48 @@
 <?php
-	include 'menu.php'
+include 'menu.php'
 ?>
+<div class="container-fluid">
+	<h2>Ranking</h2>
+	<ul class="nav nav-tabs">
+		<li class="active"><a data-toggle="pill" href="#geral"><button type="button" class="btn btn-primary">Geral</button></a></li>
+		<li><a data-toggle="pill" href="#materia"><button type="button" class="btn btn-primary">Por matéria</button></a></li>
+	</ul>
+	<div class="tab-content">
+		<div id="geral" class="tab-pane fade in active">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>Pontuação</th>
+						<th>Posição</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div id="materia" class="tab-pane fade">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>Pontuação</th>
+						<th>Posição</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</body>
+	</html>
