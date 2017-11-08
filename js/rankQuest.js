43e41b775620609bea20898d64dcede07ed04ea2
garabini97 <gabriel.garabinimg@gmail.com>
2017-11-07 23:32:52 -0200

    RESET
    RESET

--- js/rankQuest.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("#menu-toggle").click(function(e) {
-		e.preventDefault();
-		$("#wrapper").toggleClass("toggled");
-		$("#container-fluid").toggleClass("container-fluid");
-		$("#container-fluid").toggleClass("container-fluid-toggled");
-	});
-})
--- processa/processa_logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo '<script>top.location.href="../pages/landing.php";</script>';
-?>
